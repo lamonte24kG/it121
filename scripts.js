84c5128449a3a95557f81bc2351500fc54c04d00
lamonte24kG <141269004+lamonte24kG@users.noreply.github.com>
2024-01-06 11:11:58 -0800

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,6 @@
+
+let firstName = "William ";
+let lastName = "Golden ";
+let preferName = "LaMonte pronounced \'le mont\' ";
+let yearsOfStudy = "2nd year in the Web Design program ";
+let goal = "becoming a Full Stack Developer";
