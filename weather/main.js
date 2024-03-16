ea5ffa4ea38f5e206300ed796f6a90380dad8338
lamonte24kG <141269004+lamonte24kG@users.noreply.github.com>
2024-03-15 23:20:36 -0700

    Add files via upload

--- weather/main.js
+++ weather/main.js
@@ -15,13 +15,18 @@ fetch('https://api.openweathermap.org/data/2.5/forecast?q='+newName.value+'&appi
     }
 console.log( data.list[i]);
     for(i = 0; i<5; i++){
-        document.getElementById("day" + (i+1) + "Max").innerHTML = "Max: " + Number(data.list[i].main.temp_max).toFixed(2) + "°";
+        document.getElementById("day" + (i+1) + "Max").innerHTML = "Max: " + Number(data.list[i].main.temp_max).toFixed(1) + "°";
     }
+
+    for(i = 0; i<5; i++){
+        document.getElementById("day" + (i+1) + "Hum").innerHTML = "Humidity: " + Number(data.list[i].main.humidity).toFixed(1) + "%";
+    }
+    
     //------------------------------------------------------------
 
     //Getting Weather Icons
      for(i = 0; i<5; i++){
-        document.getElementById("img" + (i+1)).src = "http://openweathermap.org/img/wn/"+
+        document.getElementById("img" + (i+1)).src = "http://openweathermap.org/img/wn/@2x.png"+
         data.list[i].weather[0].icon
         +".png";
     }
@@ -58,7 +63,49 @@ function CheckDay(day){
     for(i = 0; i<5; i++){
         document.getElementById("day" + (i+1)).innerHTML = weekday[CheckDay(i)];
     }
-  
+    //------------------------------------------------------------
 
 
+/*
+document.getElementById("day1Min").innerHTML = Math.round(data.list[0].main.temp_min - 273.15, -2);
+document.getElementById("day2Min").innerHTML = Math.round(data.list[1].main.temp_min - 273.15, -2);
+document.getElementById("day3Min").innerHTML = Math.round(data.list[2].main.temp_min - 273.15, -2);
+document.getElementById("day4Min").innerHTML = Math.round(data.list[3].main.temp_min - 273.15, -2);
+document.getElementById("day5Min").innerHTML = Math.round(data.list[4].main.temp_min - 273.15, -2);*/
 
+/*document.getElementById("day1Max").innerHTML = Math.round(data.list[0].main.temp_max - 273.15, -2);
+document.getElementById("day2Max").innerHTML = Math.round(data.list[0].main.temp_max - 273.15, -2);
+document.getElementById("day3Max").innerHTML = Math.round(data.list[0].main.temp_max - 273.15, -2);
+document.getElementById("day4Max").innerHTML = Math.round(data.list[0].main.temp_max - 273.15, -2);
+document.getElementById("day5Max").innerHTML = Math.round(data.list[0].main.temp_max - 273.15, -2);*/
+
+/*document.getElementById("img1").src = "http://openweathermap.org/img/w/"+
+data.list[0].weather[0].icon
++".png";
+document.getElementById("img2").src = "http://openweathermap.org/img/w/"+
+data.list[1].weather[0].icon
++".png";
+document.getElementById("img3").src = "http://openweathermap.org/img/w/"+
+data.list[2].weather[0].icon
++".png";
+document.getElementById("img4").src = "http://openweathermap.org/img/w/"+
+data.list[3].weather[0].icon
++".png";
+document.getElementById("img5").src = "http://openweathermap.org/img/w/"+
+data.list[4].weather[0].icon
++".png";*/
+
+/*
+document.getElementById("day1").innerHTML = weekday[CheckDay(0)];
+document.getElementById("day2").innerHTML = weekday[CheckDay(1)];
+document.getElementById("day3").innerHTML = weekday[CheckDay(2)];
+document.getElementById("day4").innerHTML = weekday[CheckDay(3)];
+document.getElementById("day5").innerHTML = weekday[CheckDay(4)];*/
+
+/*weekday[0] = "Sunday";
+weekday[1] = "Monday";
+weekday[2] = "Tuesday";
+weekday[3] = "Wednesday";
+weekday[4] = "Thursday";
+weekday[5] = "Friday";
+weekday[6] = "Saturday";*/
